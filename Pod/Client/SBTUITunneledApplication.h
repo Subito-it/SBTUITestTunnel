3c67f0212802bf3c0f9fea36173b98ba8f403312
Tomas Camin <tomas.camin@scmitaly.it>
2016-05-12 14:49:49 +0200

    Podspec issue

--- Pod/Client/SBTUITunneledApplication.h
+++ Pod/Client/SBTUITunneledApplication.h
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@import XCTest;
+#import <XCTest/XCTest.h>
 
 #import "SBTUITestTunnel.h"
 
