6721a8689aeaf2aaa7d2fdcb1efbf56ddb87fae4
Tomas Camin <tomas.camin@scmitaly.it>
2017-12-18 14:18:12 +0100

    Add targets for dynamic framework build

---
+++ Example/Pods/SBTUITestTunnel_Client_Framework/SBTUITestTunnel_Client_Framework.h
@@ -0,0 +1,18 @@
+//
+//  SBTUITestTunnel_Client_Framework.h
+//  SBTUITestTunnel_Client_Framework
+//
+//  Created by Tomas Camin on 18/12/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBTUITestTunnel_Client_Framework.
+FOUNDATION_EXPORT double SBTUITestTunnel_Client_FrameworkVersionNumber;
+
+//! Project version string for SBTUITestTunnel_Client_Framework.
+FOUNDATION_EXPORT const unsigned char SBTUITestTunnel_Client_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBTUITestTunnel_Client_Framework/PublicHeader.h>
+
+
---
+++ Example/Pods/SBTUITestTunnel_Server_Framework/SBTUITestTunnel_Server_Framework.h
@@ -0,0 +1,18 @@
+//
+//  SBTUITestTunnel_Server_Framework.h
+//  SBTUITestTunnel_Server_Framework
+//
+//  Created by Tomas Camin on 18/12/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBTUITestTunnel_Server_Framework.
+FOUNDATION_EXPORT double SBTUITestTunnel_Server_FrameworkVersionNumber;
+
+//! Project version string for SBTUITestTunnel_Server_Framework.
+FOUNDATION_EXPORT const unsigned char SBTUITestTunnel_Server_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBTUITestTunnel_Server_Framework/PublicHeader.h>
+
+
