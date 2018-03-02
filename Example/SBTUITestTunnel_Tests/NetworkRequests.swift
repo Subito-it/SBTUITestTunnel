1d37ca6af3f2906bf7eac786dd0b877ad857ddd0
Tomas Camin <tomas.camin@scmitaly.it>
2018-03-02 13:43:39 +0100

    Tests step 3

--- Example/SBTUITestTunnel_Tests/NetworkRequests.swift
+++ Example/SBTUITestTunnel_Tests/NetworkRequests.swift
@@ -8,7 +8,11 @@
 
 import Foundation
 
-class NetworkRequests {
+class NetworkRequests: NSObject {
+    fileprivate var done: Bool = false
+    fileprivate var sessionData: Data? = nil
+    fileprivate var sessionResponse: HTTPURLResponse? = nil
+    
     private func returnDictionary(status: Int?, headers: [String: String]? = [:], data: Data?) -> [String: Any] {
         return ["responseCode": status ?? 0,
                 "responseHeaders": headers ?? [:],
@@ -26,6 +30,10 @@ class NetworkRequests {
         return false
     }
     
+    func returnCode(_ result: [String: Any]) -> Int {
+        return result["responseCode"] as? Int ?? -1
+    }
+    
     func dataTaskNetwork(urlString: String, httpMethod: String = "GET", httpBody: String? = nil, delay: TimeInterval = 0.0) -> [String: Any] {
         var retData: Data! = nil
         var retResponse: HTTPURLResponse! = nil
@@ -38,7 +46,7 @@ class NetworkRequests {
             request.httpBody = httpBody.data(using: .utf8)
         }
         
-        var done = false
+        done = false
         URLSession.shared.dataTask(with: request) {
             data, response, error in
             DispatchQueue.main.async {
@@ -46,7 +54,7 @@ class NetworkRequests {
                 retHeaders = retResponse.allHeaderFields as! [String: String]
                 retData = data
 
-                done = true
+                self.done = true
             }
         }.resume()
         
@@ -69,7 +77,7 @@ class NetworkRequests {
             request.httpBody = "The http body".data(using: .utf8)
         }
         
-        var done = false
+        done = false
         URLSession.shared.uploadTask(with: request, from: data) {
             data, response, error in
             DispatchQueue.main.async {
@@ -77,7 +85,7 @@ class NetworkRequests {
                 retHeaders = retResponse.allHeaderFields as! [String: String]
                 retData = data
                 
-                done = true
+                self.done = true
             }
         }.resume()
         
@@ -87,46 +95,55 @@ class NetworkRequests {
         
         return returnDictionary(status: retResponse.statusCode, headers: retHeaders, data: retData)
     }
-//
-//    func downloadTaskNetwork(urlString: String, data: Data, httpMethod: String, httpBody: Bool = false, delay: TimeInterval = 0.0, shouldPushResult: Bool = true) {
-//        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + delay) { [weak self] in
-//            let sem = DispatchSemaphore(value: 0)
-//
-//            let url = URL(string: urlString)!
-//            var request = URLRequest(url: url)
-//            request.httpMethod = httpMethod
-//            if httpBody {
-//                request.httpBody = "The http body".data(using: .utf8)
-//            }
-//
-//            var retData: Data! = nil
-//            var retResponse: HTTPURLResponse! = nil
-//            var retHeaders: [String: String]! = nil
-//
-//            URLSession.shared.downloadTask(with: request) {
-//                dataUrl, response, error in
-//
-//                retResponse = response as! HTTPURLResponse
-//                retHeaders = retResponse.allHeaderFields as! [String: String]
-//                if let dataUrl = dataUrl {
-//                    retData = try? Data(contentsOf: dataUrl)
-//                }
-//
-//                sem.signal()
-//                }
-//                .resume()
-//
-//            sem.wait()
-//
-//            if shouldPushResult {
-//                DispatchQueue.main.async { [weak self] in
-//                    let retDict = self?.returnDictionary(status: retResponse.statusCode, headers: retHeaders, data: retData) ?? [:]
-//                    self?.performSegue(withIdentifier: "networkSegue", sender: try! JSONSerialization.data(withJSONObject: retDict, options: .prettyPrinted))
-//                }
-//            }
-//        }
-//    }
-//
+
+    func downloadTaskNetwork(urlString: String, data: Data, httpMethod: String, httpBody: Bool = false, delay: TimeInterval = 0.0) -> [String: Any] {
+        var retData: Data! = nil
+        var retResponse: HTTPURLResponse! = nil
+        var retHeaders: [String: String]! = nil
+        
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod
+        if httpBody {
+            request.httpBody = "The http body".data(using: .utf8)
+        }
+        
+        done = false
+        URLSession.shared.downloadTask(with: request) {
+            dataUrl, response, error in
+            DispatchQueue.main.async {
+                retResponse = response as! HTTPURLResponse
+                retHeaders = retResponse.allHeaderFields as! [String: String]
+                if let dataUrl = dataUrl {
+                    retData = try? Data(contentsOf: dataUrl)
+                }
+                
+                self.done = true
+            }
+        }.resume()
+        
+        while !done {
+            RunLoop.main.run(until: Date(timeIntervalSinceNow: 1.0))
+        }
+        
+        return returnDictionary(status: retResponse.statusCode, headers: retHeaders, data: retData)
+    }
+
+    //TODO
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 //    func backgroundDataTaskNetwork(urlString: String, data: Data, httpMethod: String, httpBody: Bool = false, delay: TimeInterval = 0.0, shouldPushResult: Bool = true) {
 //        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + delay) { [weak self] in
 //            self?.sessionSemaphore = DispatchSemaphore(value: 0)
@@ -210,4 +227,21 @@ class NetworkRequests {
 //            }
 //        }
 //    }
+}
+
+
+
+extension NetworkRequests: URLSessionTaskDelegate, URLSessionDataDelegate {
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        done = true
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        sessionData?.append(data)
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        sessionResponse = response as? HTTPURLResponse
+    }
 }
