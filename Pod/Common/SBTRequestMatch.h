5b0473cb6884d4211633b7c7a9b6149c069af016
Tomas Camin <tomas.camin@adevinta.com>
2019-12-12 17:36:36 +0100

    Fix method documentation

--- Pod/Common/SBTRequestMatch.h
+++ Pod/Common/SBTRequestMatch.h
@@ -66,7 +66,7 @@
  *  @param url a regex that is matched against the request url
  *  @param query an array of a regex that are matched against the request query (params in GET and DELETE, body in POST and PUT). Instance will match if all regex are fulfilled. You can specify that a certain query should not match by prefixing it with an exclamation mark `!`
  *  @param method HTTP method
- *  @body a regex that is matched against the request body
+ *  @param body a regex that is matched against the request body
  */
 - (nonnull instancetype)initWithURL:(nonnull NSString *)url query:(nonnull NSArray<NSString *> *)query method:(nonnull NSString *)method body:(nonnull NSString *)body;
 
