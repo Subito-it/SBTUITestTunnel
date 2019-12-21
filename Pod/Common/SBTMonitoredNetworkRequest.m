5f64d0df5ab1602b19e50d1e322e7ce37ae8b89b
Tomas Camin <tomas.camin@adevinta.com>
2019-12-21 15:14:39 +0100

    Format

--- Pod/Common/SBTMonitoredNetworkRequest.m
+++ Pod/Common/SBTMonitoredNetworkRequest.m
@@ -15,9 +15,9 @@
 // limitations under the License.
 
 #if DEBUG
-#ifndef ENABLE_UITUNNEL
-#define ENABLE_UITUNNEL 1
-#endif
+    #ifndef ENABLE_UITUNNEL
+        #define ENABLE_UITUNNEL 1
+    #endif
 #endif
 
 #if ENABLE_UITUNNEL
