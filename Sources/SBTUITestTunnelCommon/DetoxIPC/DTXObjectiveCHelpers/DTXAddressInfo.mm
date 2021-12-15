8c0476e52e77a0dd288bf3079a393492e5600d73
Johannes Plunien <joplunien@ebay.com>
2021-12-15 14:07:19 +0100

    Add missing preprocessor macro in DTXAddressInfo

--- Sources/SBTUITestTunnelCommon/DetoxIPC/DTXObjectiveCHelpers/DTXAddressInfo.mm
+++ Sources/SBTUITestTunnelCommon/DetoxIPC/DTXObjectiveCHelpers/DTXAddressInfo.mm
@@ -6,6 +6,14 @@
 //  Copyright © 2017-2020 Wix. All rights reserved.
 //
 
+#if DEBUG
+    #ifndef ENABLE_UITUNNEL
+        #define ENABLE_UITUNNEL 1
+    #endif
+#endif
+
+#if ENABLE_UITUNNEL
+
 #import "DTXAddressInfo.h"
 #include <dlfcn.h>
 #include <cxxabi.h>
@@ -118,3 +126,5 @@ static char* (*__dtx_swift_demangle)(const char *mangledName,
 }
 
 @end
+
+#endif
