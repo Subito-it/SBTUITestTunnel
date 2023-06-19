d489e7beeeef1780e461d9223cec4ee2dde3ee07
Tomas Camin <tomas.camin@adevinta.com>
2023-06-19 11:05:52 +0200

    Remove conditional compilation

--- Example/SBTUITestTunnel/SBTAppDelegate.m
+++ Example/SBTUITestTunnel/SBTAppDelegate.m
@@ -16,68 +16,64 @@
 
 #import "SBTAppDelegate.h"
 
-#if DEBUG
-    @import SBTUITestTunnelServer;
-    @import CoreLocation;
-#endif
+@import SBTUITestTunnelServer;
+@import CoreLocation;
 
 @implementation SBTAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    #if DEBUG
-        [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturnNil" block:^NSObject *(NSObject *object) {
-            [[NSUserDefaults standardUserDefaults] setObject:object forKey:@"custom_command_test"];
-            [[NSUserDefaults standardUserDefaults] synchronize];
+    [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturnNil" block:^NSObject *(NSObject *object) {
+        [[NSUserDefaults standardUserDefaults] setObject:object forKey:@"custom_command_test"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
 
-            return nil;
+        return nil;
+    }];
+    [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturn123" block:^NSObject *(NSObject *object) {
+        [[NSUserDefaults standardUserDefaults] setObject:object forKey:@"custom_command_test"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+
+        return @"123";
+    }];
+    #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 140000
+    if (@available(iOS 14.0, *)) {
+        [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturnCLAccuracyAuth" block:^NSObject *(NSObject *object) {
+            CLLocationManager *manager = [CLLocationManager new];
+            return [@(manager.accuracyAuthorization) stringValue];
         }];
-        [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturn123" block:^NSObject *(NSObject *object) {
-            [[NSUserDefaults standardUserDefaults] setObject:object forKey:@"custom_command_test"];
-            [[NSUserDefaults standardUserDefaults] synchronize];
-
-            return @"123";
+        [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturnCLAuthStatus" block:^NSObject *(NSObject *object) {
+            return [@([CLLocationManager authorizationStatus]) stringValue];
         }];
-        #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 140000
-        if (@available(iOS 14.0, *)) {
-            [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturnCLAccuracyAuth" block:^NSObject *(NSObject *object) {
-                CLLocationManager *manager = [CLLocationManager new];
-                return [@(manager.accuracyAuthorization) stringValue];
-            }];
-            [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturnCLAuthStatus" block:^NSObject *(NSObject *object) {
-                return [@([CLLocationManager authorizationStatus]) stringValue];
-            }];
 
-            [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturnUNAuthRequest" block:^NSObject *(NSObject *object) {
-                dispatch_semaphore_t sema = dispatch_semaphore_create(0);
-                __block BOOL authGranted;
-                [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions: UNAuthorizationOptionNone completionHandler:^(BOOL granted, NSError * _Nullable error) {
-                    authGranted = granted;
-                    dispatch_semaphore_signal(sema);
-                }];
-
-                dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
-                return [@(authGranted) stringValue];
+        [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturnUNAuthRequest" block:^NSObject *(NSObject *object) {
+            dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+            __block BOOL authGranted;
+            [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions: UNAuthorizationOptionNone completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                authGranted = granted;
+                dispatch_semaphore_signal(sema);
             }];
 
-            [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturnUNAuthStatus" block:^NSObject *(NSObject *object) {
-                dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+            dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+            return [@(authGranted) stringValue];
+        }];
 
-                __block UNNotificationSettings *notificationSettings;
-                [[UNUserNotificationCenter currentNotificationCenter] getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
-                    notificationSettings = settings;
-                    dispatch_semaphore_signal(sema);
-                }];
+        [SBTUITestTunnelServer registerCustomCommandNamed:@"myCustomCommandReturnUNAuthStatus" block:^NSObject *(NSObject *object) {
+            dispatch_semaphore_t sema = dispatch_semaphore_create(0);
 
-                dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
-                return [@(notificationSettings.authorizationStatus) stringValue];
+            __block UNNotificationSettings *notificationSettings;
+            [[UNUserNotificationCenter currentNotificationCenter] getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+                notificationSettings = settings;
+                dispatch_semaphore_signal(sema);
             }];
-        }
-        #endif
-    
-        BOOL didTakeOff = [SBTUITestTunnelServer takeOff];
-        NSLog(@"Tunnel established: %d", didTakeOff);
+
+            dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+            return [@(notificationSettings.authorizationStatus) stringValue];
+        }];
+    }
     #endif
+
+    BOOL didTakeOff = [SBTUITestTunnelServer takeOff];
+    NSLog(@"Tunnel established: %d", didTakeOff);
     
     return YES;
 }
