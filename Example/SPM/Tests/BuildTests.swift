cc7e75b22a807a0bf25d8ce72f083f11a7362039
Tomas Camin <tomas.camin@adevinta.com>
2023-10-13 10:44:08 +0200

    Add sample integration

---
+++ Example/SPM/Tests/BuildTests.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+import SBTUITestTunnelClient
+import XCTest
+
+final class BuildTest: XCTestCase {    
+    func testTargetBuild() {
+        let request = SBTRequestMatch(url: "https://www.subito.it")
+        XCTAssertNotNil(request)
+    }
+}
