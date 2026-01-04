627bdd168ebbe0e8af2d84437a1949b29949cc01
Marco Pagliari <lechuckcaptain@protonmail.com>
2026-01-04 17:46:05 +0100

    Update commandScrollScrollViewWithIdentifier to detect SwiftUI views

--- Sources/SBTUITestTunnelServer/SBTUITestTunnelServer.m
+++ Sources/SBTUITestTunnelServer/SBTUITestTunnelServer.m
@@ -1047,7 +1047,33 @@ static NSTimeInterval SBTUITunneledServerDefaultTimeout = 60.0;
                     while (!result && !cancelled) {
                         NSArray *allScrollViewViews = [scrollView allSubviews];
                         for (UIView *scrollViewView in [allScrollViewViews reverseObjectEnumerator]) {
-                            BOOL expectedTargetIdentifier = [scrollViewView.accessibilityIdentifier isEqualToString:targetElementIdentifier] || [scrollViewView.accessibilityLabel isEqualToString:targetElementIdentifier];
+                            BOOL matchIdentifier = [scrollViewView.accessibilityIdentifier isEqualToString:targetElementIdentifier];
+                            BOOL matchLabel = [scrollViewView.accessibilityLabel isEqualToString:targetElementIdentifier];
+                            
+                            // Use key-value coding as a fallback to access the `accessibilityIdentifier`.
+                            // This is necessary for SwiftUI views, which are wrapped in a `UIHostingController`
+                            // and don't directly expose an `accessibilityIdentifier`.
+                            // https://github.com/cashapp/AccessibilitySnapshot/blob/main/Sources/AccessibilitySnapshot/Parser/Swift/Classes/AccessibilityHierarchyParser.swift#L887-L892
+                            BOOL matchAccessibilityElementIdentifier = NO;
+                            NSArray *accessibilityElements = scrollViewView.accessibilityElements;
+                            if (accessibilityElements) {
+                                for (NSObject *accessibilityElement in accessibilityElements) {
+                                    @try {
+                                        // Use KVC to access accessibilityIdentifier
+                                        // Some accessibility elements may not support KVC for this property
+                                        NSString *accessibilityIdentifier = [accessibilityElement valueForKey:@"accessibilityIdentifier"];
+                                        if (accessibilityIdentifier && [accessibilityIdentifier isEqualToString:targetElementIdentifier]) {
+                                            matchAccessibilityElementIdentifier = YES;
+                                            break;
+                                        }
+                                    } @catch (NSException *exception) {
+                                        // Element doesn't support KVC for accessibilityIdentifier, skip it
+                                        continue;
+                                    }
+                                }
+                            }
+                            
+                            BOOL expectedTargetIdentifier = matchIdentifier || matchLabel || matchAccessibilityElementIdentifier;
                             
                             if (expectedTargetIdentifier) {
                                 CGFloat targetContentOffsetX = scrollView.contentOffset.x;
